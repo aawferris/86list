04c56ad1e1e9a7b65d63a581e55914ac10e07f94
Andrew <aawferris@gmail.com>
2020-11-30 16:41:02 -0500

    controllers

---
+++ eightysixlist/app/controllers/posts_controller.rb
@@ -0,0 +1,65 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :add_post]
+  before_action :authorize_request, only: [:create, :update, :destroy]
+  before_action :set_user_post, only: [:update, :destroy]
+
+  # GET /foods
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    render json: @post #, include: :flavors
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    @post.user = @current_user
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+  end
+
+  def add_comment
+    @comment = Comment.find(params[:comment_id])
+    @post.flavors << @comment
+
+    render json: @post, include: :comments
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @post = Post.find(params[:id])
+    end
+
+    def set_user_food
+      @post = @current_user.posts.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def post_params
+      params.require(:post).permit(:name, :user_id)
+    end
+end
