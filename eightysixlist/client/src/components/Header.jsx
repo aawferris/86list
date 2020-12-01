2f14d8cf1164a874cb4a6cefd8d524fa7524d439
Andrew <aawferris@gmail.com>
2020-12-01 15:26:32 -0500

    added header.jsx

---
+++ eightysixlist/client/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header(props) {
+  const { currentUser, handleLogout } = props;
+  return (
+    <div>
+      <h1>86list</h1>
+      {
+        currentUser ?
+          <>
+            <p>{currentUser.username}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+          :
+          <Link to='/login'>Login/Register</Link>
+      }
+      <hr />
+      {
+        currentUser &&
+        <>
+          <Link to='/restaurants'>foods</Link>
+          <Link to='/posts'>flavors</Link>
+        </>
+      }
+    </div>
+  )
+}
