2350b240912252c94453c78ad1e75b89010d32f6
Andrew <aawferris@gmail.com>
2020-12-09 13:23:16 -0500

    add redirect

--- eightysixlist/client/src/services/apiConfig.js
+++ eightysixlist/client/src/services/apiConfig.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? /* link to your heroku app. Example:*/ "https://eightysixlist-api.herokuapp.com/"
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
